f9224224403abcab7970751d7a027fc17f413239
Robin Geldolf <robin.geldolf@appreciate.be>
2015-03-02 14:24:47 +0100

    detail activity possible fix (unable to reproduce)

--- UiTagenda/src/main/java/org/uitagenda/DetailActivity.java
+++ UiTagenda/src/main/java/org/uitagenda/DetailActivity.java
@@ -398,17 +398,19 @@ public class DetailActivity extends ActionBarActivity {
     }
 
     private void stripUnderlines(TextView textView) {
-        if (textView.getText().length() > 0) {
-            Spannable s = (Spannable) textView.getText();
-            URLSpan[] spans = s.getSpans(0, s.length(), URLSpan.class);
-            for (URLSpan span : spans) {
-                int start = s.getSpanStart(span);
-                int end = s.getSpanEnd(span);
-                s.removeSpan(span);
-                span = new URLSpanNoUnderline(span.getURL());
-                s.setSpan(span, start, end, 0);
+        if(textView.getText() != "") {
+            if (textView.getText().length() > 0) {
+                Spannable s = (Spannable) textView.getText();
+                URLSpan[] spans = s.getSpans(0, s.length(), URLSpan.class);
+                for (URLSpan span : spans) {
+                    int start = s.getSpanStart(span);
+                    int end = s.getSpanEnd(span);
+                    s.removeSpan(span);
+                    span = new URLSpanNoUnderline(span.getURL());
+                    s.setSpan(span, start, end, 0);
+                }
+                textView.setText(s);
             }
-            textView.setText(s);
         }
     }
 
